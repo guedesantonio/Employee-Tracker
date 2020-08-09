f0eba6d64e46b0df5022534e0f92f18c50ff1358
Antonio Guedes <gguedesantonio@gmail.com>
2020-08-09 18:10:34 +1000

    Schema.sql TABLES created

--- schema.sql
+++ schema.sql
@@ -16,6 +16,16 @@ CREATE TABLE department (
 CREATE TABLE role (
   id int NOT NULL AUTO_INCREMENT,
   title varchar(30) NOT NULL,
-  salary DECIMAL(10,2) NULL,
-  department_id INT
-  PRIMARY KEY (id);
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+  id int NOT NULL AUTO_INCREMENT,
+  first_name varchar(30) NOT NULL,
+  last_name varchar(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL,
+    PRIMARY KEY (id)
+)
